f7363139d9e1c310fe3f1459909fb05e2f7c4295
Batuhan Ceylan <zekbatuhan@gmail.com>
2021-01-27 11:05:34 +0300

    v2

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+Ceveats:
+
+Handling redirects are not guaranteed.
+
+
+Tested with Python 3.8.7 on macOS 10.15.7
--- crawler.py
+++ crawler.py
@@ -1,145 +1,214 @@
-import sys
-import json
-from urllib.parse import urlparse
-from threading import Thread, Lock, active_count
+from urllib.parse import urlsplit
+from threading import Thread, Lock
 from queue import Queue
 import pprint
 from time import sleep
 import logging
-# import os
 
-
+import click
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
+from validator_collection import checkers
 
-REQUESTS_TIMEOUT = 10
+REQUESTS_TIMEOUT = 3
 REQUESTS_RETRY_COUNT = 3
 REQUESTS_BACKOFF_FACTOR = 1
 
 endpoints = dict()
-to_be_crawled = Queue()
+to_crawl = Queue()
 already_crawled = set()
+crawl_set = set()
 lock = Lock()
 
 
-def find_links_in_html(request):
+def insert_url_into_endpoints_dict(url, url2=None):
+    if url not in endpoints:
+        endpoints[url] = set()
+    if url2 is not None:
+        endpoints[url].add(url2)
+
+
+def insert_url_into_crawl_target_set(url):
+    if url not in crawl_set:
+        crawl_set.add(url)
+        to_crawl.put(url)
+
+
+def is_link_interesting(this_link, base_url, url, logger):
+    if this_link is None:
+        return
+    try:
+        if this_link.startswith('/'):
+            endpoints[url].add(base_url+this_link[1:])
+            insert_url_into_crawl_target_set(base_url+this_link[1:])
+        elif this_link.startswith(base_url):
+            endpoints[url].add(this_link)
+            insert_url_into_crawl_target_set(this_link)
+        # else:
+        #     logger.warning(f'Out-of-scope link: {str(this_link)}')
+    except Exception as e:
+        logger.error(f'(is_link_interesting): {str(e)}. Link: \
+                     {str(this_link)}')
+
+
+def find_links_in_html(request, base_url, all_links, logger):
     url = request.url
     html_text = request.text
-    parsed_uri = urlparse(url)
-    base_url = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-    if endpoints.get(base_url, None) is None:
-        endpoints[base_url] = set()
+    parsed_url = urlsplit(url)
+
+    if not parsed_url.geturl().startswith(base_url):
+        return
+
+    insert_url_into_endpoints_dict(parsed_url.geturl())
 
     soup = BeautifulSoup(html_text, 'html.parser')
-    for link in soup.find_all('a'):
-        this_link = link.get('href')
-        # print(this_link)
-        try:
-            if not this_link.endswith('/'):
-                this_link = this_link + '/'
-            if this_link.startswith('/'):
-                # print(this_link)
-                endpoints[url].add(base_url+this_link[1:])
-                if not (base_url+this_link[1:] in endpoints):
-                    # print(url+this_link[1:])
-                    endpoints[base_url+this_link[1:]] = set()
-                    to_be_crawled.put(base_url+this_link[1:])
-            elif this_link.startswith(base_url):
-                endpoints[url].add(this_link)
-                if not (this_link in endpoints):
-                    # print(this_link)
-                    endpoints[this_link] = set()
-                    to_be_crawled.put(this_link)
-        except Exception as e:
-            print(f'Error (find_links_in_html): {str(e)}')
+    if all_links:
+        for link in soup.find_all(href=True):
+            this_link = link.get('href')
+            is_link_interesting(this_link, base_url, url, logger)
+    else:
+        for link in soup.find_all('a'):
+            this_link = link.get('href')
+            is_link_interesting(this_link, base_url, url, logger)
 
 
-def crawl():
-    # s = requests.Session()
-    # retry = Retry(total=REQUESTS_RETRY_COUNT, backoff_factor=REQUESTS_BACKOFF_FACTOR)
-    # adapter = HTTPAdapter(max_retries=retry)
-    # s.mount('http://', adapter)
-    # s.mount('https://', adapter)
+def crawl(base_url, all_links, logger):
+    logger.debug(f'Starting thread with base url: {base_url}')
+    # Start by opening a session.
     with requests.Session() as s:
-        retry = Retry(total=REQUESTS_RETRY_COUNT, backoff_factor=REQUESTS_BACKOFF_FACTOR)
+        # Set request retry policy.
+        retry = Retry(total=REQUESTS_RETRY_COUNT,
+                      backoff_factor=REQUESTS_BACKOFF_FACTOR)
         adapter = HTTPAdapter(max_retries=retry)
         s.mount('http://', adapter)
         s.mount('https://', adapter)
         while True:
-            if not to_be_crawled.qsize()>0:
+            # Stop thread execution if there's no crawl target left.
+            if not to_crawl.qsize() > 0:
                 break
 
-            lock.acquire()
-            url = to_be_crawled.get()
-            if url in already_crawled:
-                continue
+            # Acquire a lock to prevent multiple threads doing the same job.
+            if lock.acquire(timeout=5):
+                url = to_crawl.get()
+                # Check whether the url was crawled before.
+                if url in already_crawled:
+                    lock.release()
+                    continue
+                else:
+                    already_crawled.add(url)
+                    lock.release()
+
+                try:
+                    # Perform request. Check for errors.
+                    # If it's okay dig inside for links.
+                    r = s.get(url, timeout=REQUESTS_TIMEOUT)
+                    r.raise_for_status()
+                    find_links_in_html(r, base_url, all_links, logger)
+                except Exception as e:
+                    logger.warning(f'(crawl): {str(e)}')
             else:
-                already_crawled.add(url)
-            lock.release()
-        
-            try:
-                r = s.get(url, timeout=REQUESTS_TIMEOUT)
-                r.raise_for_status()
-                # if r.status_code == 
-                find_links_in_html(r)
-            except Exception as e:
-                print(f'Error (crawl): {str(e)}')
+                logger.debug('Could not acquire lock')
+                pass
 
+            # Prevent flooding target web server.
             sleep(1)
+    logger.debug('Ending thread.')
 
 
-if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-        num_threads = sys.argv[2]
-    except Exception as e:
-        # url = 'https://duckduckgo.com/'
-        # url = 'https://monzo.com/'
-        url = 'https://www.ismercuryinretrograde.com/'
-        # url = 'https://monzo.com/i/business'
-        num_threads = 1
+@click.command()
+@click.option('--nthreads', default=5, help='Number of threads',
+              show_default=5)
+@click.option('--output', default='endpoints_tree.txt',
+              help='Output file location', show_default='endpoints_tree.txt')
+@click.option('--all-links', is_flag=True)
+@click.argument('url')
+def crawler(nthreads=None, url=None, output=None, all_links=None):
+    """
+    Web crawler starts from URL to all found links under the same netloc.
+    """
+    # Create logger object
+    logger = logging.getLogger('crawler')
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('crawler.log')
+    file_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s' +
+                                  ' - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-    # logger = logging.getLogger('crawler')
-    # logger.setLevel(logging.DEBUG)
-    # file_handler = logging.FileHandler('crawler.log')
-    # file_handler.setLevel(logging.DEBUG)
-    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # file_handler.setFormatter(formatter)
-    # logger.addHandler(file_handler)
+    # Check if URL is valid
+    url = url
+    if not checkers.is_url(url):
+        logger.error(f'The url you have entered is not valid. URL: {str(url)}')
+        exit(1)
 
-    print(f'URL: {str(url)}')
-    print(f'Number of threads: {str(num_threads)}')
+    num_threads = nthreads
+    output = output
+    all_links = all_links
 
-    to_be_crawled.put(url)
+    logger.info('Starting crawler...')
+    logger.info(f'URL: {str(url)}')
+    logger.info(f'Number of threads: {str(num_threads)}')
+    logger.info(f'Output: {str(output)}')
 
+    # Requests follows redirections so we need to set initial base_url outside.
+    parsed_url = urlsplit(url)
+    base_url = f'{parsed_url.scheme}://{parsed_url.netloc}/'
+
+    # Place URL into a queue.
+    to_crawl.put(url)
+
+    # Create threads.
     crawler_threads = list()
-    for i in range(int(num_threads)):
-        crawler_threads.append(Thread(target=crawl))
+    for _ in range(int(num_threads)):
+        crawler_threads.append(Thread(target=crawl, args=(base_url, all_links,
+                                      logger,)))
 
+    # Start first thread than wait some time for first crawl to complete.
+    # Afterwards start remaining threads.
     first_thread = True
-
     for thread in crawler_threads:
         thread.start()
         if first_thread:
             sleep(REQUESTS_TIMEOUT)
             first_thread = False
 
-    # print(active_count())
-
+    # Wait for all threads to finish.
     for thread in crawler_threads:
         thread.join()
-    
+
+    # Print the result to a file and also to standard output.
+    print_results_to_file(endpoints, output, logger)
+    print_results_to_stdout(endpoints, logger)
+
+    return
+
+
+def print_results_to_stdout(local_endpoints_dict, logger):
+    if not isinstance(local_endpoints_dict, dict):
+        logger.error('local_endpoints_dict is not a dictionary. Type: ' +
+                     f'{type(local_endpoints_dict)}')
+        return
+
+    for key in local_endpoints_dict.keys():
+        print(f'Links found on \'{str(key)}\':')
+        for value in local_endpoints_dict[key]:
+            print(f'\t- {value}')
+
+
+def print_results_to_file(endpoints, output_file_name, logger):
     try:
-        with open('endpoints_tree.txt', 'w') as f:
+        with open(output_file_name, 'w') as f:
             pp = pprint.PrettyPrinter(indent=4, stream=f)
             pp.pprint(endpoints)
-        with open('endpoints_list.txt', 'w') as f:
-            pp = pprint.PrettyPrinter(indent=4, stream=f)
-            pp.pprint(already_crawled)
+        logger.info('Successfully wrote output file.')
     except Exception as e:
-        print(f'Error (main): {str(e)}')
+        logger.error(f'(print_results_to_file): {str(e)}')
 
+
+if __name__ == '__main__':
+    crawler()
     exit(0)
